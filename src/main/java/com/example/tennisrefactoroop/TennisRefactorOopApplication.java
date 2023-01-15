50d02c8f288ea53cc08b33937ae4dab731418b9d
Onyetech <ikennajamesokonkwo@gmail.com>
2023-01-15 22:56:36 +0100

    set up project jdk

---
+++ src/main/java/com/example/tennisrefactoroop/TennisRefactorOopApplication.java
@@ -0,0 +1,13 @@
+package com.example.tennisrefactoroop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisRefactorOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisRefactorOopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tennisrefactoroop/TennisRefactorOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tennisrefactoroop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisRefactorOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
