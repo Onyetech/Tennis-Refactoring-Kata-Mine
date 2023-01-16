d3ed009908808dcb08fe113e44983ea11337c8b9
Onyetech <ikennajamesokonkwo@gmail.com>
2023-01-16 05:54:07 +0100

    refactoring TennisGame3

--- src/main/java/com/example/tennisrefactoroop/TennisGame3.java
+++ src/main/java/com/example/tennisrefactoroop/TennisGame3.java
@@ -2,8 +2,8 @@ package com.example.tennisrefactoroop;
 
 public class TennisGame3 implements TennisGame {
     
-    private int p2;
-    private int p1;
+    private int playerTwoScore = 0;
+    private int playerOneScore = 0;
     private String p1N;
     private String p2N;
 
@@ -14,23 +14,23 @@ public class TennisGame3 implements TennisGame {
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (playerOneScore < 4 && playerTwoScore < 4 && !(playerOneScore + playerTwoScore == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            s = p[playerOneScore];
+            return (playerOneScore == playerTwoScore) ? s + "-All" : s + "-" + p[playerTwoScore];
         } else {
-            if (p1 == p2)
+            if (playerOneScore == playerTwoScore)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = playerOneScore > playerTwoScore ? p1N : p2N;
+            return ((playerOneScore - playerTwoScore)*(playerOneScore - playerTwoScore) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
     
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.playerOneScore += 1;
         else
-            this.p2 += 1;
+            this.playerTwoScore += 1;
         
     }
 
